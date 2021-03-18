560ecb2c523f57a9d29dd2df1ab4d6500a287c90
Matheus Schneider <matheus.ssm@gmail.com>
2021-03-18 09:06:57 -0300

    refactored logic, added debounce function

--- movie-fight/script.js
+++ movie-fight/script.js
@@ -11,15 +11,20 @@ const fetchData = async (searchTerm) => {
 
 const input = document.querySelector('input');
 
-let timeoutId;
-const onInput = (event) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-
-  timeoutId = setTimeout(() => {
-    fetchData(event.target.value);
-  }, 500);
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
 };
+
+const onInput = debounce((event) => {
+  fetchData(event.target.value);
+}, 500);
 
 input.addEventListener('input', onInput);
