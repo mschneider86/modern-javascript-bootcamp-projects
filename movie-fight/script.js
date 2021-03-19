674ea7a5132e2f516444d01cbd4bc4596c0bb8fd
Matheus Schneider <matheus.ssm@gmail.com>
2021-03-19 09:48:49 -0300

    added click event to result items

--- movie-fight/script.js
+++ movie-fight/script.js
@@ -49,6 +49,11 @@ const onInput = async (event) => {
       ${movie.Title}
     `;
 
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+    });
+
     resultsWrapper.appendChild(option);
   }
 };
