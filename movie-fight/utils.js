23c7e998b7c90c9051e7abf63c5d11d7f5bbe654
Matheus Schneider <matheus.ssm@gmail.com>
2021-04-20 08:55:28 -0300

    update: refactoring autocomplete component

--- movie-fight/utils.js
+++ movie-fight/utils.js
@@ -9,3 +9,16 @@ const debounce = (func, delay = 1000) => {
     }, delay);
   };
 };
+
+let links0 = document.querySelectorAll('a');
+for (link of links) {
+  const linkText = link.innerText.toLowerCase();
+  console.log(link);
+  if (
+    linkText.includes('cabine de descompressão') ||
+    linkText.includes('eliminação') ||
+    linkText.includes('eliminado')
+  ) {
+    link.linkText = '';
+  }
+}
