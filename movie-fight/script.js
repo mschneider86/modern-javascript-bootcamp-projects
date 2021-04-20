fcc2c7fd5ebaeb544bc2ce8b54fb75d4b296bee2
Matheus Schneider <matheus.ssm@gmail.com>
2021-04-20 09:01:14 -0300

    update: autocomplete component refactoring

--- movie-fight/script.js
+++ movie-fight/script.js
@@ -1,19 +1,4 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: '12827b40',
-      s: searchTerm,
-    },
-  });
-
-  if (response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
-};
-
-createAutoComplete({
+const fetchData = createAutoComplete({
   root: document.querySelector('.autocomplete'),
   renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
@@ -27,6 +12,20 @@ createAutoComplete({
   },
   inputValue(movie) {
     return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: '12827b40',
+        s: searchTerm,
+      },
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
   },
 });
 
--- movie-fight/utils.js
+++ movie-fight/utils.js
@@ -9,16 +9,3 @@ const debounce = (func, delay = 1000) => {
     }, delay);
   };
 };
-
-let links0 = document.querySelectorAll('a');
-for (link of links) {
-  const linkText = link.innerText.toLowerCase();
-  console.log(link);
-  if (
-    linkText.includes('cabine de descompressão') ||
-    linkText.includes('eliminação') ||
-    linkText.includes('eliminado')
-  ) {
-    link.linkText = '';
-  }
-}
