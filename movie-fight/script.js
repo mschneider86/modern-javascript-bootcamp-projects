0ed1116188ee51bb95c07b6e3f9ca8e17a32e3f9
Matheus Schneider <matheus.ssm@gmail.com>
2021-04-23 09:20:44 -0300

    added variables to select attributes to compare

--- movie-fight/script.js
+++ movie-fight/script.js
@@ -70,6 +70,23 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 const runComparison = () => {};
 
 const movieTemplate = (movieDetail) => {
+  const dollars = parseInt(
+    movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, '')
+  );
+
+  const metascore = parseInt(movieDetail.Metascore);
+  const imdbRating = parseFloat(movieDetail.imdbRating);
+  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+  const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+    const value = parseInt(word);
+
+    if (isNaN(value)) {
+      return prev;
+    } else {
+      return prev + value;
+    }
+  }, 0);
+
   return `
     <article class="media">
       <figure class="media-left>
